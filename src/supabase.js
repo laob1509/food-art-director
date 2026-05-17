afccee7c9b527245e37ad355602f1a90ecf74316
Luiz Alfredo <luiz1509@gmail.com>
2026-05-17 02:06:37 -0300

    Fix URL Supabase correta

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yclvvfapkdwltayiuiivy.supabase.co'
+const supabaseUrl = 'https://yclvvfapkdwltayuiivy.supabase.co'
 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljbHZ2ZmFwa2R3bHRheXVpaXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NzExMDMsImV4cCI6MjA5NDU0NzEwM30.O4fuDlL_KGPPO9WHaazrOtbkfO5KVpbVwpND5sVH0IA'
 
