1c8d61175de287c647f12aebc13533533e0b479a
Luiz Alfredo <luiz1509@gmail.com>
2026-05-17 01:51:30 -0300

    Fix chave anon Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://yclvvfapkdwltayiuiivy.supabase.co'
 
-const supabaseKey = 'sb_publishable_TWC_5XNeYcksnMg5i6Grkw_DKqtG_K_'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljbHZ2ZmFwa2R3bHRheXVpaXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NzExMDMsImV4cCI6MjA5NDU0NzEwM30.O4fuDlL_KGPPO9WHaazrOtbkfO5KVpbVwpND5sVH0IA'
 
 export const supabase = createClient(
   supabaseUrl,
